c63d9a8eae0c47d11e0c92a2658d72c7609f6e03
MrAik3ro <61144642+mraikero-01@users.noreply.github.com>
2022-02-28 23:58:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const discord = require("discord.js-selfbot-v11")
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 0
 
 // const port = 5000
 
